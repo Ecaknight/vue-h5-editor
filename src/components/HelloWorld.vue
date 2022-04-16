daa4f7f1e6b8a6cdbd73320d023b666f210d8485
Ecaknight <1561557640@qq.com>
2022-04-17 00:48:16 +0800

    初始化项目

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="hello">测试less是否生效</div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<style lang="less" scoped>
+.hello {
+  width: 100%;
+  height: 700px;
+  background: #fcc;
+}
+</style>
